ef6b5152442ef77e0954407272c07cc468ef86ed
keshav <keshav.kattel@waterflow.technology>
2025-08-30 20:47:57 +0545

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,40 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc';
+import checkFile from 'eslint-plugin-check-file';
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: import.meta.dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    'next/core-web-vitals', // Next.js default rules for performance and best practices
+    'next/typescript', // TypeScript support
+    'prettier', // Disable ESLint formatting rules that conflict with Prettier
+  ),
   {
     ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
+      'node_modules/**',
+      '.next/**',
+      'out/**',
+      'build/**',
+      'next-env.d.ts',
     ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    plugins: {
+      'check-file': checkFile,
+    },
+    rules: {
+      'no-console': 'error', // Block console logs in production
+      'react/no-array-index-key': 'error', // Prevent using index as a key
+      'no-debugger': 'error', // Disallow debugger statements
+      'check-file/filename-naming-convention': [
+        'error',
+        { '**/*.{js,jsx,ts,tsx}': 'KEBAB_CASE' }, // Enforce kebab-case filenames
+        { ignoreMiddleExtensions: true }, // Allow names like `component.test.tsx`
+      ],
+    },
   },
 ];
 
